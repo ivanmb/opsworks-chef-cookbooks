bfca5b46d6945423ee0c0358ba45eac93055c8bb
Iván Badgen <ivanbadgen@gmail.com>
2013-10-31 18:13:48 -0300

    Using repository .htaccess

--- symfony/recipes/configure.rb
+++ symfony/recipes/configure.rb
@@ -18,21 +18,21 @@ node[:deploy].each do |application, deploy|
   end
 
   # Place environment variables in the .htaccess file in the web-root 
-  template "#{deploy[:deploy_to]}/current/web/.htaccess" do
-    source "htaccess.erb"
-    owner deploy[:user] 
-    group deploy[:group]
-    mode "0660"
+  #template "#{deploy[:deploy_to]}/current/web/.htaccess" do
+  #  source "htaccess.erb"
+  #  owner deploy[:user] 
+  #  group deploy[:group]
+  #  mode "0660"
 
-    variables( 
-        :env => (node[:custom_env] rescue nil), 
-        :application => "#{application}" 
-    )
+  #  variables( 
+  #      :env => (node[:custom_env] rescue nil), 
+  #      :application => "#{application}" 
+  #  )
 
-    only_if do
-     File.directory?("#{deploy[:deploy_to]}/current/web")
-    end
-  end
+  #  only_if do
+  #   File.directory?("#{deploy[:deploy_to]}/current/web")
+  #  end
+  #end
   
   
   script "install_composer" do

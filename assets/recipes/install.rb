e2bf44e7fda53e2a84d41d967b688bb41bb06a2f
Iván Badgen <ivanbadgen@gmail.com>
2013-10-31 16:46:04 -0300

    Asset management for post deploy

---
+++ assets/recipes/install.rb
@@ -0,0 +1,11 @@
+node[:deploy].each do |application, deploy|
+  script "install_assets" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    bash node_installation.sh
+    bash assets.sh
+    EOH
+  end
+end 

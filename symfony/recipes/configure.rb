f2dbaf87277e2d2be021612a874565fd13369c86
Iván Badgen <ivanbadgen@gmail.com>
2014-04-22 13:44:07 -0300

    Setting up cronjobs

--- symfony/recipes/configure.rb
+++ symfony/recipes/configure.rb
@@ -11,30 +11,30 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     mkdir -p app/cache app/logs
-    mount -o remount,acl /srv/www 
+    mount -o remount,acl /srv/www
     setfacl -R -m u:www-data:rwX -m u:ubuntu:rwX app/cache/ app/logs/
     setfacl -dR -m u:www-data:rwx -m u:ubuntu:rwx app/cache/ app/logs/
     EOH
   end
 
-  # Place environment variables in the .htaccess file in the web-root 
+  # Place environment variables in the .htaccess file in the web-root
   #template "#{deploy[:deploy_to]}/current/web/.htaccess" do
   #  source "htaccess.erb"
-  #  owner deploy[:user] 
+  #  owner deploy[:user]
   #  group deploy[:group]
   #  mode "0660"
 
-  #  variables( 
-  #      :env => (node[:custom_env] rescue nil), 
-  #      :application => "#{application}" 
+  #  variables(
+  #      :env => (node[:custom_env] rescue nil),
+  #      :application => "#{application}"
   #  )
 
   #  only_if do
   #   File.directory?("#{deploy[:deploy_to]}/current/web")
   #  end
   #end
-  
-  
+
+
   script "install_composer" do
     interpreter "bash"
     user "root"
@@ -65,7 +65,18 @@ node[:deploy].each do |application, deploy|
 #    EOH
 #  end
 
-   
+  script "setup_cron" do
+    interpreter "bash"
+    user "root"
+    code <<-EOH
+      { \
+       echo "*/1 * * * * php #{deploy[:deploy_to]}/current/app/console swiftmailer:spool:send --message-limit=50 --env=prod" && \
+       echo "30 4 * * * php #{deploy[:deploy_to]}/current/app/console micursada:notification-remind" \
+      ;} | crontab -u www-data -
+    EOH
+  end
+
+
   include_recipe 'symfony::paramconfig'
   include_recipe 'symfony::logrotate'
 

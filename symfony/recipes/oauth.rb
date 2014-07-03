240ca2cfafc60a28ed1531081adb6ca09335a1c1
Iván Badgen <ivanbadgen@gmail.com>
2014-07-03 16:34:16 -0300

    Added node iteration

--- symfony/recipes/oauth.rb
+++ symfony/recipes/oauth.rb
@@ -1,23 +1,26 @@
-script "oauth_setup" do
-  interpreter "bash"
-  user "root"
-  cwd "/"
-  code <<-EOH
-  apt-get install php-pear php5-dev libpcre3-dev
-  pecl install oauth 
-  EOH
-end
+node[:deploy].each do |application, deploy|
 
-template 'oauth.ini' do
-  case node[:platform]
-  when 'centos','redhat','fedora','amazon'
-    path "/etc/php.d/oauth.ini"
-  when 'debian','ubuntu'
-    path "/etc/php5/conf.d/oauth.ini"
+  script "oauth_setup" do
+    interpreter "bash"
+    user "root"
+    cwd "/"
+    code <<-EOH
+    pecl install oauth 
+    EOH
   end
-  source 'oauth.ini.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-  notifies :restart, resources(:service => 'apache2')
+
+  template 'oauth.ini' do
+    case node[:platform]
+    when 'centos','redhat','fedora','amazon'
+      path "/etc/php.d/oauth.ini"
+    when 'debian','ubuntu'
+      path "/etc/php5/conf.d/oauth.ini"
+    end
+    source 'oauth.ini.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+    notifies :restart, resources(:service => 'apache2')
+  end
+
 end
